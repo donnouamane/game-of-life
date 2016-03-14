b14a34f90a27d4ebb75ac119bbacefa458774241
Nouaman Harti <nouaman.harti@hotmail.com>
2016-03-14 00:08:00 +0000

    udpate script

--- patchTemplate/generate_patch.sh
+++ patchTemplate/generate_patch.sh
@@ -6,3 +6,5 @@ export PATCH_VERSION="2016.1"
 sed -e "s/\${patchVersion}/$PATCH_VERSION/" install.sh > install_patch_$PATCH_VERSION.sh
 sed -e "s/\${patchVersion}/$PATCH_VERSION/" rollback.sh > rollback_patch_$PATCH_VERSION.sh
 rm -f generate_patch.sh install.sh rollback.sh
+cd ..
+mv patchTemplate patch-PATCH_VERSION
