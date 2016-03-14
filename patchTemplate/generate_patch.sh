5868347acb8bf5a935ff09401223572f56e3a57b
Nouaman Harti <nouaman.harti@hotmail.com>
2016-03-14 20:35:31 +0000

    update script

--- patchTemplate/generate_patch.sh
+++ patchTemplate/generate_patch.sh
@@ -3,10 +3,18 @@ mkdir source
 mkdir backup
 sed -e "s/\${patchVersion}/$PATCH_VERSION/" install.sh > install_patch_$PATCH_VERSION.sh
 sed -e "s/\${patchVersion}/$PATCH_VERSION/" rollback.sh > rollback_patch_$PATCH_VERSION.sh
+#set +x
+#insert source here
+commitArray = ($COMMITS)
+for variable in commitArray
+do
+echo "commit: $variable"
+done
+#set -x
 rm -f generate_patch.sh install.sh rollback.sh
 cd ..
 PATCH_NAME=patch-$PATCH_VERSION
 mv patchTemplate $PATCH_NAME
 tar czvf $PATCH_NAME.tar.gz $PATCH_NAME
 rm -rf $PATCH_NAME
-set +x
+
