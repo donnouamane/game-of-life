02ee405e97b5385e53634f4dfed711c83a3e11fd
Nouaman Harti <nouaman.harti@hotmail.com>
2016-03-15 20:51:29 +0000

    add release/txt

--- patchTemplate/generate_patch.sh
+++ patchTemplate/generate_patch.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 mkdir patchTemplate/source
 mkdir patchTemplate/backup
+declare -A hashMap
 for commit in $COMMITS
 do
-key=$(git show -s --format=%ct $commit)
-git diff-tree -r --no-commit-id --name-only --diff-filter=ACMRT $commit | xargs git checkout $commit
-git diff-tree -r --no-commit-id --name-only --diff-filter=ACMRT $commit | xargs tar cvf patchTemplate/source/$key.tar --remove-files
+    key=$(git show -s --format=%ct $commit)
+    hashMap["$key"]=$commit
+    git diff-tree -r --no-commit-id --name-only --diff-filter=ACMRT $commit | xargs git checkout $commit
+    git diff-tree -r --no-commit-id --name-only --diff-filter=ACMRT $commit | xargs tar cf patchTemplate/source/$key.tar --remove-files
 done
-cd patchTemplate
+cd  patchTemplate/source
+echo      "FILES GROUPED BY COMMIT'S MESSAGE FOR PATCH $PATCH_VERSION" | tee ../release.txt
+echo      ""  | tee -a ../release.txt
+for file in `ls`; do echo $(basename "$file" .tar); done | sort -n |
+while read f
+do
+    echo      ""
+    echo      ""
+    git show -s --format=%B ${hashMap["$f"]} 2>&1 | tee -a ../release.txt
+    tar -tf "$f.tar" >> ../release.txt
+    tar xvf "$f.tar" && rm "$f.tar"
+done
+cd ..
 sed -e "s/\${patchVersion}/$PATCH_VERSION/" install.sh > install_patch_$PATCH_VERSION.sh
 sed -e "s/\${patchVersion}/$PATCH_VERSION/" rollback.sh > rollback_patch_$PATCH_VERSION.sh
 for variable in $COMMITS
